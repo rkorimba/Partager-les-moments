5dd947a3479481bf3c8a80ee0608c10d5e5c532f
rkorimba <korimbacus.riad@gmail.com>
2018-05-17 11:39:24 +0200

    Fonction miseEnPlace

--- Partager les moments/PhotoController.swift
+++ Partager les moments/PhotoController.swift
@@ -13,11 +13,29 @@ class PhotoController: UIViewController {
     @IBOutlet weak var partagerBouton: UIBarButtonItem!
     @IBOutlet weak var photoAPartager: UIImageView!
     @IBOutlet weak var texteAPartager: UITextView!
+   
+    let texteVide = "Entrez un texte ..."
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        miseEnPlace()
     }
 
+    func miseEnPlace() {
+        
+        photoAPartager.contentMode = .scaleAspectFit
+        photoAPartager.image = #imageLiteral(resourceName: "Superman-facebook.svg")
+        photoAPartager.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(prendrePhoto))
+        photoAPartager.addGestureRecognizer(tap)
+        
+        texteAPartager.text = texteVide
+    }
+    
+    @objc func prendrePhoto() {
+        
+    }
+    
     @IBAction func boutonPartageAppuye(_ sender: Any) {
     }
 }
