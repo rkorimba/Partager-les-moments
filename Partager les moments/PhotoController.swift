28ca793376725f759f44dfe86c086632eb7c5727
rkorimba <korimbacus.riad@gmail.com>
2018-05-17 11:32:54 +0200

    Création du projet

---
+++ Partager les moments/PhotoController.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoController.swift
+//  Partager les moments
+//
+//  Created by Riad Korimbacus on 17/05/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import UIKit
+
+class PhotoController: UIViewController {
+
+    @IBOutlet weak var partagerBouton: UIBarButtonItem!
+    @IBOutlet weak var photoAPartager: UIImageView!
+    @IBOutlet weak var texteAPartager: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func boutonPartageAppuye(_ sender: Any) {
+    }
+}
